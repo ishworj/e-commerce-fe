fbe9b244c78fb13f34e11c6005a07dfe4bdedc42
Ishwor karki <ishworkarkij11@gmail.com>
2025-04-05 20:37:54 +1100

    initial redux store and slices

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store/reduxStore.js";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
-  </StrictMode>
+    </Provider>
+  </BrowserRouter>
 );
