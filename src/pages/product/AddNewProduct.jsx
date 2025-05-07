3195299d6d6905d9bd6a1c049f57cb1d71996e1e
Ishwor karki <ishworkarkij11@gmail.com>
2025-05-07 21:32:18 +1000

    add new book working, multiple file upload

--- src/pages/product/AddNewProduct.jsx
+++ src/pages/product/AddNewProduct.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { UserLayout } from "../../components/layouts/UserLayout";
 import { Link, useNavigate } from "react-router-dom";
 import { Button, Form } from "react-bootstrap";
@@ -6,7 +6,12 @@ import { productInputs } from "../../assets/form-data/ProductInput";
 import CustomInput from "../../components/custom inputs/CustomInput";
 import useForm from "../../hooks/useForm";
 import { useDispatch, useSelector } from "react-redux";
-import { createProductAction } from "../../features/products/productActions";
+import {
+  createProductAction,
+  getAdminProductAction,
+  getPublicProductAction,
+} from "../../features/products/productActions";
+import { MdDelete } from "react-icons/md";
 const initialState = {};
 
 const AddNewProduct = () => {
@@ -14,10 +19,46 @@ const AddNewProduct = () => {
   const navigate = useNavigate();
   const { form, handleOnChange } = useForm(initialState);
   const { Categories } = useSelector((state) => state.categoryInfo);
+
+  const [images, setImages] = useState([]);
+  const [previews, setPreviews] = useState([]);
+  const imageRef = useRef(null);
+
+  const handleImageChange = (e) => {
+    const newFiles = Array.from(e.target.files);
+    // Combine existing and new images
+    if (newFiles.length + images.length <= 4) {
+      const updatedFiles = [...images, ...newFiles];
+      setImages(updatedFiles);
+      // Generate and append previews
+      const newPreviews = newFiles.map((file) => URL.createObjectURL(file));
+      setPreviews((prev) => [...prev, ...newPreviews]);
+    } else {
+      alert("you can only upload 4 images, ");
+      imageRef.current.value = "";
+    }
+  };
+
+  const handleOnImageDelete = (index) => {
+    setImages(images.filter((_, i) => i !== index));
+    setPreviews(previews.filter((_, i) => i !== index));
+    imageRef.current.value = null;
+  };
+
   const handleOnSubmit = async (e) => {
-    e.preventDefault()
-    const success = await dispatch(createProductAction(form));
+    e.preventDefault();
+    const formData = new FormData();
+    Object.entries(form).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+    // Append images
+    images.forEach((image) => {
+      formData.append("images", image);
+    });
+
+    const success = await dispatch(createProductAction(formData));
     if (success) {
+      getPublicProductAction();
       navigate("/admin/products");
     }
   };
@@ -58,6 +99,45 @@ const AddNewProduct = () => {
               ))}
             </Form.Select>
           </Form.Group>
+
+          {/* Image Upload */}
+          <Form.Group className="mb-3" controlId="images">
+            <Form.Label>Upload Images</Form.Label>
+            <Form.Control
+              type="file"
+              name="images"
+              multiple
+              accept=".jpg,.jpeg,.png"
+              onChange={handleImageChange}
+              ref={imageRef}
+            />
+          </Form.Group>
+
+          {/* Image Previews */}
+          {previews.length > 0 && (
+            <>
+              <div className="d-flex flex-wrap gap-2 mb-3">
+                {previews.map((src, index) => (
+                  <div className="d-flex  flex-column position-relative ">
+                    <MdDelete
+                      onClick={() => handleOnImageDelete(index)}
+                      className="cursor-pointer text-danger position-absolute end-0 "
+                    />
+                    <img
+                      key={index}
+                      src={src}
+                      alt="Preview"
+                      style={{
+                        width: "100px",
+                        height: "100px",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
 
           <div className="d-grid">
             <Button type="submit"> Submit new Product </Button>
